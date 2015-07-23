e8b224d29873c47899fa3e2684a87896a8fd9ea0
duncanmorris <duncan.morris@distilled.net>
2015-07-23 14:47:30 +0100

    Update wsgi.py

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
+import os
 import newrelic.agent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+NEW_RELIC_INI = os.path.join(BASE_DIR, 'newrelic.ini')
+newrelic.agent.initialize(NEW_RELIC_INI)
 
-# NB in Staging the NEW_RELIC_ENVIRONMENT variable must be set to 'staging'
-# in order for it to use the staging settings of the newrelic.ini
-newrelic.agent.initialize('/python/{{ project_name }}/newrelic.ini')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
