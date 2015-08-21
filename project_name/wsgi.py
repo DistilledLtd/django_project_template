fc77b756af2cc6679fdf6e6883dd692468542562
duncanmorris <duncan.morris@distilled.net>
2015-08-21 11:44:03 +0100

    Update wsgi.py

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+
+if not os.environ.get('DJANGO_SETTINGS_MODULE'):
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+
 import newrelic.agent
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 NEW_RELIC_INI = os.path.join(BASE_DIR, 'newrelic.ini')
